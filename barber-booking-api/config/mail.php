7504f190419beb94d8d352d50b74c8b32ce894d9
mechti-razi <razzymechti@gmail.com>
2026-07-19 15:41:07 +0100

    fix: use built-in smtp mailer with SendGrid defaults (SENDGRID_API_KEY)

--- barber-booking-api/config/mail.php
+++ barber-booking-api/config/mail.php
@@ -38,15 +38,13 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'transport'  => 'smtp',
+            'host'       => env('MAIL_HOST', 'smtp.sendgrid.net'),
+            'port'       => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username'   => env('MAIL_USERNAME', 'apikey'),
+            'password'   => env('MAIL_PASSWORD', env('SENDGRID_API_KEY')),
+            'timeout'    => null,
         ],
 
         'ses' => [
